6f9a830b3a9521480c392d2bedb19e64cac310b8
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-14 03:13:25 +0000

    rename dict to dict2fgl

--- lande/fermi/data/catalogs.py
+++ lande/fermi/data/catalogs.py
@@ -1,8 +1,4 @@
-from os.path import join
-
-def get_2fgl():
-    catalog_basedir = "/afs/slac/g/glast/groups/catalog/P7_V4_SOURCE"
-    d = dict(ft2=join(catalog_basedir,"ft2_2years.fits"),
-             ltcube=join(catalog_basedir,"ltcube_24m_pass7.4_source_z100_t90_cl0.fits")
-    )
-    return d
+""" Store paths to important catalog data. 
+"""
+dict2fgl = dict(ft2="/afs/slac/g/glast/groups/catalog/P7_V4_SOURCE/ft2_2years.fits",
+                ltcube="/afs/slac/g/glast/groups/catalog/P7_V4_SOURCE/ltcube_24m_pass7.4_source_z100_t90_cl0.fits")
