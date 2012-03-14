0ef826d3cd7aa628cb696fffd416f756f947e277
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-14 03:13:09 +0000

    Make Shreekumar an IsotropicConstant

--- lande/fermi/likelihood/diffuse.py
+++ lande/fermi/likelihood/diffuse.py
@@ -2,7 +2,7 @@ import numpy as np
 import re
 from os.path import basename
 
-from skymaps import IsotropicSpectrum, IsotropicPowerLaw
+from skymaps import IsotropicSpectrum, IsotropicPowerLaw, IsotropicConstant
 
 from uw.like.Models import PowerLaw, Constant
 from uw.like.roi_diffuse import DiffuseSource
@@ -65,18 +65,20 @@ def get_background(*args):
 
     return bg[0] if len(args)==1 else bg
 
-def get_sreekumar(diff_factor=1, scaling_model=None):
-
-    if scaling_model is None: 
-        scaling_model = Constant()
+def get_sreekumar(diff_factor=1, free=(True, False)):
 
     # use Sreekumar-like defaults
     if diff_factor == 1:
         name = 'Sreekumar Isotropic'
     else:
         name = 'Sreekumar Isotropic x%s' % diff_factor
+
+    free = np.asarray(free).copy()
+    model = PowerLaw(index=2.1, free=free)
+    model.set_flux(1.5e-5*diff_factor, emin=100, emax=np.inf)
+
     return DiffuseSource(
         name=name,
-        diffuse_model=IsotropicPowerLaw(1.5e-5*diff_factor,2.1),
-        scaling_model=scaling_model)
+        diffuse_model=IsotropicConstant(),
+        scaling_model=model)
 
