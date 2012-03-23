a70a0b303133b0025a6c08d7c601e5c9ee499185
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-23 02:16:03 +0000

    improve

--- lande/utilities/simtools.py
+++ lande/utilities/simtools.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 from os.path import join, expandvars, exists
 from os import makedirs
-import collections
-
+from collections import defaultdict
 from itertools import product
 
+import yaml
+
 from uw.utilities import keyword_options 
+
 from . lists import flatten
+from . files import locate
+from . save import savedict
 
 class SimBuilder(object):
 
@@ -25,6 +29,10 @@ class SimBuilder(object):
 
     def build(self):
         if self.params is not None:
+
+            for k,v in self.params.items():
+                if not isinstance(v,list) and not isinstance(v,tuple):
+                    self.params[k] = [v]
 
             keys = flatten(self.params.keys())
             values = self.params.values()
@@ -64,3 +72,60 @@ class SimBuilder(object):
 
             submit_all = join(self.savedir,'submit_all.sh')
             open(submit_all,'w').write("submit_all */run.sh $@")
+
+
+class SimMerger(object):
+
+    def __init__(self, savedir, keys):
+        self.savedir = expandvars(savedir)
+        self.keys = keys
+
+        self._merge()
+
+    @staticmethod
+    def traverse(data, keys):
+        """ Pull out of a complicated data strucutre
+            a value where keys is a list of the 
+            keys to follow.
+
+            Example:
+
+                >>> d = dict(a=dict(b=dict(c=[0,dict(a='treasure')])))
+                >>> print SimMerger.traverse(d, ['a', 'b', 'c', 1, 'a'])
+                treasure
+        """
+        if len(keys) == 1:
+            return data[keys[0]]
+        else:
+            return SimMerger.traverse(data[keys[0]], keys[1:])
+
+    def _merge(self):
+        self.results = defaultdict(list)
+
+        all_results=locate('results_*.yaml', self.savedir)
+
+        for i,r in enumerate(all_results):
+            if i % 10==0: print '%s' % i
+
+            x = yaml.load(open(r))
+
+            if x is None: continue
+
+            for k,v in self.keys.items():
+
+                # if each reults file contains a straight array, assume
+                # that each of the entries in the results file needs to 
+                # be parsed identically. I hope this is a reasonable assumption.
+                if isinstance(x,list):
+                    for i in x: self.results[k].append(SimMerger.traverse(i,v))
+                else:
+                    self.results[k].append(SimMerger.traverse(x,v))
+                    
+
+    def save(self, filename):
+        savedict(filename, self.results)
+        
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
