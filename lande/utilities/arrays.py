16bb319a4b26c067a37e76edf953f11ebafe3945
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-24 06:13:01 +0000

    rename function

--- lande/utilities/arrays.py
+++ lande/utilities/arrays.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def isclose(a,b, rtol=1e-05, atol=1e-08):
+def almost_equal(a,b, rtol=1e-05, atol=1e-08):
     return np.abs(a - b) <= (atol + rtol * np.abs(b))
 
 def nzip(a,b):
