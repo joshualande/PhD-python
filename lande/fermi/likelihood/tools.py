d6e9f4f5e5e918023daf7611a3477975ef7e1703
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-24 17:14:12 +0000

    more robust input

--- lande/fermi/likelihood/tools.py
+++ lande/fermi/likelihood/tools.py
@@ -6,13 +6,28 @@ from uw.like.Models import Model
 from SED import SED
 
 
-def gtlike_or_pointlike(f_gtlike, f_pointlike, like_or_roi, *args, **kwargs):
+def gtlike_or_pointlike(f_gtlike, f_pointlike, *args, **kwargs):
     """ Note, like_or_roi can be either an ROI or a spectral model. """
+
+    if 'like' in kwargs:
+        like_or_roi = kwargs.pop('like')
+    elif 'roi' in kwargs:
+        like_or_roi = kwargs.pop('roi')
+    elif 'like_or_roi' in kwargs:
+        like_or_roi = kwargs.pop('like_or_roi')
+    elif len(args) > 0:
+        args = list(args)
+        like_or_roi = args.pop(0)
+    else:
+        raise Exception("like_or_roi must be passed into function.")
+
 
     from pyLikelihood import Function
     from BinnedAnalysis import BinnedAnalysis
+    from UnbinnedAnalysis import UnbinnedAnalysis
 
     if isinstance(like_or_roi, BinnedAnalysis) or \
+       isinstance(like_or_roi, UnbinnedAnalysis) or \
        isinstance(like_or_roi,Function):
         f=f_gtlike
     elif isinstance(like_or_roi, ROIAnalysis) or \
