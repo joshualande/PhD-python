ebbfbdba8f0c515039d6492097a302ed6d4f1ab6
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-24 17:13:52 +0000

    copy _Parameter from LikelihoodState to address bounds issue

--- lande/fermi/likelihood/superstate.py
+++ lande/fermi/likelihood/superstate.py
@@ -8,9 +8,34 @@
     (b) Allows the parameter like to be passed into restore to
         restore into a new like object.
 
+    (c) fixes a bounds error bug (see LK-73).
+
 """
 import pyLikelihood
-from LikelihoodState import _Parameter
+
+
+class _Parameter(object):
+    """ Copy of pyLikelihood.LikelihoodState with my attempted fix
+        to a bounds error bug (see LK-73). """
+    def __init__(self, par):
+        self.par = par
+        self.value = par.getValue()
+        self.minValue, self.maxValue = par.getBounds()
+        self.free = par.isFree()
+        self.scale = par.getScale()
+        self.error = par.error()
+        self.alwaysFixed = par.alwaysFixed()
+    def setDataMembers(self, par=None):
+        if par is None:
+            par = self.par
+        par.setBounds(-float('inf'), float('inf'))
+        par.setValue(self.value)
+        par.setBounds(self.minValue, self.maxValue)
+        par.setFree(self.free)
+        par.setScale(self.scale)
+        par.setError(self.error)
+        par.setAlwaysFixed(self.alwaysFixed)
+
 
 class SuperState(object):
     def __init__(self, like):
