ba62c27eadd9b64ef8a54179d8261eb6e2564849
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-24 06:13:05 +0000

    new functions

---
+++ lande/utilities/lists.py
@@ -0,0 +1,28 @@
+
+def flatten(x):
+    """flatten(sequence) -> list
+
+        Taken from http://kogs-www.informatik.uni-hamburg.de/~meine/python_tricks
+
+    Returns a single, flat list which contains all elements retrieved
+    from the sequence and all recursively contained sub-sequences
+    (iterables).
+
+    Examples:
+
+        >>> [1, 2, [3,4], (5,6)]
+        [1, 2, [3, 4], (5, 6)]
+        >>> flatten([[[1,2,3], (42,None)], [4,5], [6], 7, (8,9,10)])
+        [1, 2, 3, 42, None, 4, 5, 6, 7, 8, 9, 10]"""
+
+    result = []
+    for el in x:
+        if hasattr(el, "__iter__") and not isinstance(el, basestring):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
