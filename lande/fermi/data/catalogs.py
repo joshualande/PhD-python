1e1fd78a48ba0d980d6a52407bc0124dcedc40ec
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-03-30 23:13:08 +0000

    ft1

--- lande/fermi/data/catalogs.py
+++ lande/fermi/data/catalogs.py
@@ -1,4 +1,7 @@
 """ Store paths to important catalog data. 
 """
+from glob import glob
 dict2fgl = dict(ft2="/afs/slac/g/glast/groups/catalog/P7_V4_SOURCE/ft2_2years.fits",
-                ltcube="/afs/slac/g/glast/groups/catalog/P7_V4_SOURCE/ltcube_24m_pass7.4_source_z100_t90_cl0.fits")
+                ltcube="/afs/slac/g/glast/groups/catalog/P7_V4_SOURCE/ltcube_24m_pass7.4_source_z100_t90_cl0.fits",
+                ft1=glob("/afs/slac/g/glast/groups/catalog/P7_V4_SOURCE/pass7.3_pre_source_merit_*_pass7.4_source_z100_t90_cl0.fits"),
+               )
