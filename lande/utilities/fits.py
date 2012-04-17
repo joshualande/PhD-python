afa34810e89c75d890f91bfbe000ea4635f01be8
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-04-17 00:27:33 +0000

    refactor

--- lande/utilities/fits.py
+++ lande/utilities/fits.py
@@ -1,6 +1,27 @@
 import numpy as np
 import pyfits as pf
 
+def expand_fits_header(header, factor):
+    header = header.copy()
+    header['CDELT1']/=factor
+    header['CDELT2']/=factor
+
+    header['NAXIS1']*=factor
+    header['NAXIS2']*=factor
+
+    header['CRPIX1']=header['CRPIX1']*factor - factor/2.0 + 0.5
+    header['CRPIX2']=header['CRPIX2']*factor - factor/2.0 + 0.5
+    return header
+
+def expand_fits_data(data, factor):
+    larger=list(data.shape)
+    larger[0]*=factor
+    larger[1]*=factor
+    larger_data = np.zeros(larger,dtype=data.dtype)
+    for i in range(factor):
+        for j in range(factor):
+            larger_data[i::factor,j::factor] = data
+
 def expand_fits(pf,factor,hdu=0):
     """ Create a new fits file where
         each pixel is divided into factor**2
@@ -14,25 +35,7 @@ def expand_fits(pf,factor,hdu=0):
         in ds9 and blink them for a while to convince
         yourself the function worked.
     """
+    pf = pf.copy()
 
-    h=pf[hdu].header
-    d=pf[hdu].data
-
-    h['CDELT1']/=factor
-    h['CDELT2']/=factor
-
-    h['NAXIS1']*=factor
-    h['NAXIS2']*=factor
-
-    h['CRPIX1']=h['CRPIX1']*factor - factor/2.0 + 0.5
-    h['CRPIX2']=h['CRPIX2']*factor - factor/2.0 + 0.5
-
-    larger=list(d.shape)
-    larger[0]*=factor
-    larger[1]*=factor
-    larger_array = np.zeros(larger,dtype=d.dtype)
-    for i in range(factor):
-        for j in range(factor):
-            larger_array[i::factor,j::factor] = d
-
-    pf[hdu].data=larger_array
+    pf[hdu].header = expand_fits_header(pf[hdu].header)
+    pf[hdu].data=expand_fits_data(pf[hdu].data)
