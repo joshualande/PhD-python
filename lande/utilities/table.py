a4c553b6dd6dbb9f813c45bf0243caabe2f74144
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-05-01 04:50:35 +0000

    update docs

--- lande/utilities/table.py
+++ lande/utilities/table.py
@@ -5,8 +5,9 @@ from StringIO import StringIO
 import asciitable
 
 
-def confluence_table(dict, units=None, **kwargs):
-    """ Format a dictionary (or a DefaultDict) into a confluence table. 
+def confluence_table(table_dict, units=None, **kwargs):
+    """ Format a dictionary into a confluence table. 
+        Use the DefaultDict if you care about the order of the table.
     
         >>> d=OrderedDict()
         >>> d['name']=['bright','dim']
@@ -25,15 +26,15 @@ def confluence_table(dict, units=None, **kwargs):
     """
 
     if units is not None:
-        dict = copy.deepcopy(dict)
-        for k in dict:
-            dict[k].insert(0,units.pop(k,''))
+        table_dict = copy.deepcopy(table_dict)
+        for k in table_dict:
+            table_dict[k].insert(0,units.pop(k,''))
 
     outtable=StringIO()
 
-    asciitable.write(dict, outtable,
+    asciitable.write(table_dict, outtable,
                  Writer=asciitable.FixedWidth,
-                 names=dict.keys(),
+                 names=table_dict.keys(),
                  **kwargs)
     t=outtable.getvalue()
     t=t.replace(' |','  |')
@@ -43,7 +44,24 @@ def confluence_table(dict, units=None, **kwargs):
     return '\n'.join(t)
 
 
-def latex_table(dict, units=None, latexdict=None, **kwargs):
+def latex_table(table_dict, units=None, latexdict=None, **kwargs):
+    r""" Format a dictionary into a latex table.
+         Use the DefaultDict if you care about the order of the table.
+
+            >>> d=OrderedDict()
+            >>> d['name']=['bright','dim']
+            >>> d['flux']=['large','small']
+            >>> print latex_table(d, units=dict(flux='[ergs]'))
+            \begin{deluxetable}{cc}
+            \tablehead{\colhead{name} & \colhead{flux}\\ \colhead{ } & \colhead{[ergs]}}
+            \startdata
+            bright & large \\
+            dim & small \\
+            \enddata
+            \end{deluxetable}
+
+
+    """
 
     if latexdict is None: 
         latexdict=dict()
@@ -51,15 +69,16 @@ def latex_table(dict, units=None, latexdict=None, **kwargs):
     if units is not None:
         latexdict['units']=units
 
-    outtable=StringIO.StringIO()
+    outtable=StringIO()
         
-    asciitable.write(table, outtable, 
+    asciitable.write(table_dict, outtable, 
                      Writer=asciitable.AASTex,
-                     names=table.keys(),
+                     names=table_dict.keys(),
+                     latexdict=latexdict,
                      **kwargs
                     )
     t=outtable.getvalue()
-    return t
+    return t.strip()
     
 if __name__ == "__main__":
     import doctest
