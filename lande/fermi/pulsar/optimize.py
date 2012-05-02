59efa23d15d4b3c13781b003508d2aa33ec6f363
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-05-02 16:13:55 +0000

    rename

--- lande/fermi/pulsar/optimize.py
+++ lande/fermi/pulsar/optimize.py
@@ -10,18 +10,18 @@ class OptimizePhases(object):
         best pulsations. """
 
     def __init__(self, ft1, skydir, emax,
-                 ens=np.linspace(100,1000,21),
+                 emins=np.linspace(100,1000,21),
                  rads=np.linspace(0.1,2,20),
-                 verbose=False
+                 verbose=False,
                 ):
 
         self.ft1 = ft1
         self.skydir = skydir
         self.emax = emax
 
-        stats = np.empty([len(ens),len(rads)])
+        stats = np.empty([len(emins),len(rads)])
 
-        for iemin,emin in enumerate(ens):
+        for iemin,emin in enumerate(emins):
             for irad,radius in enumerate(rads):
                 phases = get_phases(ft1, skydir, emin, emax, radius)
                 stat = hm(phases) if len(phases) > 0 else 0
@@ -31,7 +31,7 @@ class OptimizePhases(object):
         a = np.argmax(stats)
         coord_e, coord_r = np.unravel_index(a, stats.shape)
 
-        self.optimal_emin = ens[coord_e]
+        self.optimal_emin = emins[coord_e]
         self.optimal_radius = rads[coord_r]
         self.optimal_h = np.max(stats)
 
