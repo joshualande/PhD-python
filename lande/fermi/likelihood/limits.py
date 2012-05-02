52bb072a1694f774cd4991209c2ae05717dbe5c5
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-05-02 16:04:51 +0000

    fix imports

--- lande/fermi/likelihood/limits.py
+++ lande/fermi/likelihood/limits.py
@@ -13,6 +13,7 @@ from lande.pysed import units
 from . superstate import SuperState
 from . tools import gtlike_or_pointlike
 from . save import get_full_energy_range
+from . fit import gtlike_fit_only_prefactor, paranoid_gtlike_fit
 
 def gtlike_upper_limit(like, name, cl, emin=None, emax=None, 
                        flux_units='erg', **kwargs):
