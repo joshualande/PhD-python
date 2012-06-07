f2731e1b6f0f62d21d7c15db13d4d880ec9e2546
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-06-07 16:14:06 +0000

    cleanup

--- rousseau/convert_TS_sigma.py
+++ rousseau/convert_TS_sigma.py
@@ -1,13 +1,15 @@
-from math import *
+from math import sqrt
 
 
 from scipy.stats import chi2
 import scipy.special as sp
 
 
-def TS2sigma(TS,dof):
-    # one-sided Chi^2 test
+def TS2sigma(TS,dof, quiet=False):
+    """ one-sided Chi^2 test """
     pval_1 = chi2.cdf(TS, dof)
-    sigma=sqrt(2)*sp.erfinv(pval_1)
-    print "TS=%.2f\t->\t%.2f sigma"%(TS,sigma)
+    sigma=math.sqrt(2)*sp.erfinv(pval_1)
+
+    if not quiet:
+        print "TS=%.2f\t->\t%.2f sigma"%(TS,sigma)
     return sigma
