793f4fa99a34c25669e2e2accb934c52d43428c7
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-07-02 00:29:08 +0000

    update

--- lande/fermi/likelihood/models.py
+++ lande/fermi/likelihood/models.py
@@ -1,3 +1,5 @@
+from uw.utilities.parmap import LimitMapper
+
 import numpy as np
 
 import pyLikelihood
@@ -8,10 +10,45 @@ def build_gtlike_model(model):
     """ Convert a pointlike uw.like.Models.Model to a pyLikelihood spectral model object. 
 
         Convert a pointlike model to a gtlike model:
+
             >>> import pyLikelihood
             >>> from uw.like.Models import PowerLaw
             >>> pointlike_model = PowerLaw()
+
+        Add in some weird stuff to the model
+
+            >>> pointlike_model.set_limits('index', -2, 3)
+            >>> pointlike_model.set_free('Norm', False)
+        
+        You cannot convert models unless they have limits on all parameters:
+
             >>> gtlike_model = build_gtlike_model(pointlike_model)
+            Traceback (most recent call last):
+                ...
+            Exception: Unable to build gtlike model. Parameter Norm must have limits.
+
+        After setting limits, it should work
+
+            >>> pointlike_model.set_default_limits()
+            >>> gtlike_model = build_gtlike_model(pointlike_model)
+
+        Check to see if values & limits in model are correct:
+            >>> param=gtlike_model.getParam('Index')
+            >>> gvalue=param.getTrueValue()
+            >>> pvalue=pointlike_model.gtlike['togtlike'][pointlike_model.name_mapper('Index')](pointlike_model['Index'])
+            >>> np.allclose(gvalue,pvalue)
+            True
+
+            >>> lower,upper=param.getBounds()
+            >>> lower,upper=lower*param.getScale(),upper*param.getScale()
+            >>> np.allclose([lower,upper], pointlike_model.get_limits('Index'))
+            True
+
+        Test free:
+            >>> gtlike_model.getParam('Prefactor').isFree() == pointlike_model.get_free('Norm')
+            True
+            >>> gtlike_model.getParam('Index').isFree() == pointlike_model.get_free('Index')
+
 
             >>> energies = np.logspace(1, 6, 10000)
             >>> from uw.darkmatter.spectral import DMFitFunction
@@ -20,6 +57,11 @@ def build_gtlike_model(model):
             True
 
     """
+    for p in model.param_names:
+        if not isinstance(model.get_mapper(p),LimitMapper):
+            raise Exception("Unable to build gtlike model. Parameter %s must have limits." % p)
+
+
     gtlike_name = model.gtlike['name']
 
     assert gtlike_name != 'FileFunction'
@@ -28,7 +70,19 @@ def build_gtlike_model(model):
 
 
     for p,g in zip(model.param_names,model.gtlike['param_names']):
-        gtlike_model.setParam(g,model.getp_gtlike(p))
+
+        param=gtlike_model.getParam(g)
+
+        scale=model.get_scale_gtlike(p)
+        param.setScale(scale)
+
+        lower,upper=model.get_limits_gtlike(p)
+        lower,upper=lower/scale,upper/scale
+        param.setBounds(lower,upper)
+
+        param.setTrueValue(model.getp_gtlike(p))
+
+        param.setFree(model.get_free(p))
 
     for p,g in model.gtlike['extra_param_names'].items():
         gtlike_model.setParam(g,model[p])
