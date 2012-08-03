704ccc93993f455e32a504c8fff9158933f81629
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-08-03 17:28:04 +0000

    udpate

--- lande/fermi/likelihood/iterate.py
+++ lande/fermi/likelihood/iterate.py
@@ -12,15 +12,22 @@ from uw.like.Models import PowerLaw
 
 from . fit import fit_prefactor
 from . pretty import pformat
+from . localize import MinuitLocalizer
 
-def new_ps(roi, name, l, b, tsmap_kwargs=dict(size=10)):
+def new_ps(roi, name, l, b, 
+           tsmap_kwargs=dict(size=10),
+           fit_kwargs=dict(use_gradient=False),
+           print_kwargs=dict(galactic=True, maxdist=15),
+           localize=True,
+           minuit_localizer=False,
+          ):
     """ A 'throw away' convenience function to add a new source to
         the ROI, localize it, and then print out a string which can be
         used to modify an ROI to add a new source. """
 
     skydir = SkyDir(l,b,SkyDir.GALACTIC)
 
-    roi.print_summary(galactic=True)
+    roi.print_summary(**print_kwargs)
 
     emin,emax=roi.bin_edges[[0,-1]]
 
@@ -33,22 +40,27 @@ def new_ps(roi, name, l, b, tsmap_kwargs=dict(size=10)):
 
     roi.add_source(ps)
 
-    roi.print_summary(galactic=True)
+    roi.print_summary(**print_kwargs)
 
-    fit_prefactor(roi, name, use_gradient=False)
-    roi.fit(use_gradient=False)
+    fit_prefactor(roi, name, **fit_kwargs)
+    roi.fit(**fit_kwargs)
 
+    roi.print_summary(**print_kwargs)
 
-    roi.print_summary(galactic=True)
+    if localize:
+        if minuit_localizer:
+            m = MinuitLocalizer(roi, which=name, fit_kwargs=fit_kwargs)
+            m.localize()
+        else:
+            roi.localize(which=name, update=True)
 
-    roi.localize(which=name, update=True)
-    roi.fit(use_gradient=False)
+    roi.fit(**fit_kwargs)
 
-    roi.print_summary(galactic=True)
+    roi.print_summary(**print_kwargs)
 
     print roi
 
-    ts = roi.TS(which=name, quick=False)
+    ts = roi.TS(which=name, quick=False, fit_kwargs=fit_kwargs)
     print 'TS for source %s is %.1f' % (name,ts)
 
     path=os.path.abspath(sys.argv[0])
@@ -67,19 +79,23 @@ Code to create point source:
                    **tsmap_kwargs)
 
 
-def free_src(roi, name, tsmap_kwargs=dict(size=10)):
+def free_src(roi, name, 
+             tsmap_kwargs=dict(size=10),
+             fit_kwargs=dict(use_gradient=False),
+             print_kwargs=dict(galactic=True, maxdist=15),
+            ):
     """ Throw away function to free a source in the ROI, refit
         the ROI, save it to a file and make a TS map. """
 
-    roi.print_summary(galactic=True)
+    roi.print_summary(**print_kwargs)
     if isinstance(name,basestring):
         roi.modify(which=name, free=True)
     else:
         for n in name: roi.modify(which=n, free=True)
 
-    roi.fit(use_gradient=False)
+    roi.fit(**fit_kwargs)
 
-    roi.print_summary(galactic=True)
+    roi.print_summary(**print_kwargs)
 
     path=os.path.abspath(sys.argv[0])
     print """
@@ -102,14 +118,18 @@ Code to free sources:"
                    **tsmap_kwargs)
 
 
-def fix_convergence(roi, which, model, tsmap_kwargs=dict(size=10)):
+def fix_convergence(roi, which, model, 
+                    tsmap_kwargs=dict(size=10),
+                    fit_kwargs=dict(use_gradient=False),
+                    print_kwargs=dict(galactic=True, maxdist=15),
+                   ):
     """ Fix the convergence for a source. """
 
     roi.modify(which=which, model=model, keep_old_flux=False)
 
-    roi.fit(use_gradient=False)
+    roi.fit(**fit_kwargs)
 
-    roi.print_summary()
+    roi.print_summary(**print_kwargs)
     print roi
 
     path=os.path.abspath(sys.argv[0])
