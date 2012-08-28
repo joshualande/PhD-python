c9f42e3f829aa76f04c82594fd2d82d768becea8
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-08-28 21:13:56 +0000

    refactor

--- lande/fermi/likelihood/bandfitter.py
+++ lande/fermi/likelihood/bandfitter.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 from SED import SED
+
+from uw.like.Models import PowerLaw
+
 from lande.utilities.tools import tolist
 
+from . models import build_gtlike_spectrum, build_pointlike_model
 from . save import fluxdict
 from . limits import powerlaw_upper_limit
 from . fit import paranoid_gtlike_fit
@@ -12,27 +16,42 @@ from . superstate import SuperState
 class BandFitter(object):
     """ Performs a gtlike spectral analysis fitting
         the source as a power law in several independent
-        energy bins. """
+        energy bins. 
+
+        Note, this code assumes that the intial source (named 'name')
+        is a reasonable approximation to the best spectra and
+        uses that spectra as a starting value for the fit (but
+        allows the fit to vary by a factor of 10^4 in either direciton).
+        """
     ul_choices = SED.ul_choices
 
     def __init__(self, like, name, bin_edges,
                  ul_algorithm='bayesian',
                  ul_confidence=.95,
-                 powerlaw_index=2,
+                 upper_limit_index=2,
                  flux_units='erg'):
         """ Parameters:
             * like - pyLikelihood object
             * name - source to make an SED for
             * bin_edges - if specified, calculate the SED in these bins.
             * ul_algorithm - choices = 'frequentist', 'bayesian' 
-            * ul_confidence - confidence level for upper limit. """
+            * ul_confidence - confidence level for upper limit. 
+            * upper_limit_index - what index to assume when computing upper limits.
+            * flux_units - what units to report flux in 
+            """
+        self.like               = like
         self.name               = name
         self.ul_algorithm       = ul_algorithm
         self.ul_confidence      = ul_confidence
+        self.upper_limit_index     = upper_limit_index
         self.flux_units         = flux_units
 
+        source=self.like.logLike.getSource(name) 
+        self.init_spectrum=source.spectrum()
+        self.init_model=build_pointlike_model(self.init_spectrum)
+        self.init_energes = self.like.energies[[0,-1]]
 
-        if not SED.good_binning(like, bin_edges):
+        if not SED.good_binning(self.like, bin_edges):
             raise Exception("bin_edges is not commensurate with the underlying energy binning of pyLikelihood.")
             
         bin_edges = np.asarray(bin_edges)
@@ -46,18 +65,18 @@ class BandFitter(object):
 
         empty = lambda: np.empty_like(self.energy)
 
-        self.index, self.index_err=empty(), empty()
-        self.flux, self.flux_err, self.flux_ul=empty(), empty(), empty()
-        self.eflux, self.eflux_err, self.eflux_ul=empty(), empty(), empty()
+        self.index, self.index_err                = empty(), empty()
+        self.flux,   self.flux_err,  self.flux_ul = empty(), empty(), empty()
+        self.eflux, self.eflux_err, self.eflux_ul = empty(), empty(), empty()
         self.ts=empty()
 
-        self._calculate(like)
+        self._calculate()
 
-    def _calculate(self,like):
+    def _calculate(self):
         """ Compute the flux data points for each energy. """
 
-        name    = self.name
-        init_energes = like.energies[[0,-1]]
+        like         = self.like
+        name         = self.name
 
         # Freeze all sources except one to make sed of.
         all_sources = like.sourceNames()
@@ -67,39 +86,30 @@ class BandFitter(object):
 
         saved_state = SuperState(like)
 
-        like.setSpectrum(name,'PowerLaw')
-
-        # assume a canonical dnde=1e-11 at 1GeV index 2 starting value
-        dnde = lambda e: 1e-11*(e/1e3)**-2
-
-        def get(parameter):
-            return like[like.par_index(name, parameter)]
-
-        def set(parameter, value, scale, lower, upper):
-            p=get(parameter)
-            p.setBounds(-1e100, 1e100)
-            p.setScale(scale)
-            p.setTrueValue(value)
-            p.setBounds(lower, upper)
-            like.syncSrcParams(name)
-
         for i,(lower,upper) in enumerate(zip(self.lower_energy,self.upper_energy)):
+            print 'Calculating spectrum from %.0dMeV to %.0dMeV' % (lower,upper)
 
             e = np.sqrt(lower*upper)
 
-            print 'Calculating spectrum from %.0dMeV to %.0dMeV' % (lower,upper)
-            
-            set('Prefactor', dnde(e), dnde(e), 1e-10, 1e10)
-            set('Index', -2, 1, -5, 5)
-            set('Scale', e, 1, -1e-10, 1e10)
-
             like.setEnergyRange(float(lower)+1, float(upper)-1)
+
+            # build new powerlaw
+
+
+            init_dnde = self.init_model(e)
+            model = PowerLaw(norm=init_dnde, index=2, e0=e)
+            model.set_limits('norm',1e-4*init_dnde,1e4*init_dnde, scale=init_dnde)
+            model.set_limits('index',-5,5)
+            spectrum = build_gtlike_spectrum(model)
+
+            like.setSpectrum(name,spectrum)
+            like.syncSrcParams(name)
 
             paranoid_gtlike_fit(like)
 
             self.ts[i]=like.Ts(name,reoptimize=False, verbosity=4)
 
-            index=get('Index')
+            index=spectrum.getParam('Index')
             self.index[i] = index.getTrueValue()
             self.index_err[i] = index.error()*index.getScale()
 
@@ -115,7 +125,7 @@ class BandFitter(object):
 
             print 'Calculating upper limit from %.0dMeV to %.0dMeV' % (lower,upper)
             ul_dict = powerlaw_upper_limit(like, name, 
-                                           powerlaw_index=powerlaw_index,
+                                           powerlaw_index=self.upper_limit_index,
                                            cl=self.ul_confidence,
                                            emin=lower,emax=upper,
                                            flux_units=self.flux_units)
@@ -127,7 +137,7 @@ class BandFitter(object):
                 self.eflux_ul[i] = np.nan
 
         # revert to old model
-        like.setEnergyRange(*init_energes)
+        like.setEnergyRange(*self.init_energes)
         saved_state.restore()
 
     def todict(self):
@@ -155,7 +165,7 @@ class BandFitter(object):
                     value=self.index,
                     error=self.index_err),
                 TS=self.ts,
-                powerlaw_index=self.powerlaw_index,
+                upper_limit_index=self.upper_limit_index,
                 ul_confidence=self.ul_confidence,
                 )
             )
