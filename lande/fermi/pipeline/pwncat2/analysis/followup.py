e42ff00fd3e3944ab0c57d960892a6a8afab2e40
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-09-02 23:38:24 +0000

    update

--- lande/fermi/pipeline/pwncat2/analysis/followup.py
+++ lande/fermi/pipeline/pwncat2/analysis/followup.py
@@ -14,103 +14,96 @@ from skymaps import SkyDir
 from uw.like.SpatialModels import Gaussian
 
 from lande.fermi.likelihood.tools import force_gradient
-from lande.utilities.tools import parse_strip_known_args
 from lande.fermi.likelihood.variability import VariabilityTester
 from lande.fermi.likelihood.save import pointlike_dict_to_spectrum
 from lande.utilities.save import loaddict
-from lande.fermi.spectra.sed import SED
 from lande.fermi.likelihood.free import freeze_far_away, unfreeze_far_away
 
-from analyze_psr import get_args
 from setup_pwn import load_pwn
 
 
-parser = ArgumentParser()
-parser.add_argument("--hypothesis", required=True, choices=['at_pulsar', 'point', 'extended'])
-parser.add_argument("--followup", required=True, choices=['tsmaps','plots', 'gtlike', 'variability','extul', 'temp'])
-followup_args = parse_strip_known_args(parser)
+def followup_pipeline(args):
 
-hypothesis = followup_args.hypothesis
-followup = followup_args.followup
+    hypothesis = args.hypothesis
+    followup = args.followup
 
-args = get_args()
-do_cutoff = not args.no_cutoff
+    do_cutoff = not args.no_cutoff
 
-force_gradient(use_gradient=args.use_gradient)
+    force_gradient(use_gradient=args.use_gradient)
 
-name = args.name
+    name = args.name
 
-roi = load_pwn('roi_%s_%s.dat' % (hypothesis,name))
+    roi = load_pwn('roi_%s_%s.dat' % (hypothesis,name))
 
 
-if followup == 'gtlike':
+    if followup == 'gtlike':
 
-    cutoff = do_cutoff and hypothesis in ['at_pulsar', 'point']
-    upper_limit = hypothesis=='at_pulsar'
-    if cutoff:
-        pointlike_results = loaddict('results_%s_pointlike.yaml' % name)
-        model1=pointlike_results[hypothesis]['pointlike']['test_cutoff']['model_1']
-        model1=pointlike_dict_to_spectrum(model1)
-        model1.set_default_limits(oomp_limits=True)
-    else:
-        model1=None
+        cutoff = do_cutoff and hypothesis in ['at_pulsar', 'point']
+        upper_limit = hypothesis=='at_pulsar'
+        if cutoff:
+            pointlike_results = loaddict('results_%s_pointlike.yaml' % name)
+            model1=pointlike_results[hypothesis]['pointlike']['test_cutoff']['model_1']
+            model1=pointlike_dict_to_spectrum(model1)
+            model1.set_default_limits(oomp_limits=True)
+        else:
+            model1=None
 
-    results = {hypothesis:{}}
-    results[hypothesis]['gtlike']=gtlike_analysis(roi, name=name,
-                                                  max_free = args.max_free,
-                                                  hypothesis=hypothesis, 
-                                                  upper_limit=upper_limit,
-                                                  cutoff=cutoff,
-                                                  model1=model1,
-                                                 )
+        results = {hypothesis:{}}
+        results[hypothesis]['gtlike']=gtlike_analysis(roi, name=name,
+                                                      max_free = args.max_free,
+                                                      hypothesis=hypothesis, 
+                                                      upper_limit=upper_limit,
+                                                      cutoff=cutoff,
+                                                      model1=model1,
+                                                     )
 
-    save_results(results,'results_%s_%s_%s.yaml' % (name,followup,hypothesis))
+        save_results(results,'results_%s_%s_%s.yaml' % (name,followup,hypothesis))
 
-if followup in ['plots','tsmaps']:
+    if followup in ['plots','tsmaps']:
 
-    if not os.path.exists('plots'): 
-        os.makedirs('plots')
+        if not os.path.exists('plots'): 
+            os.makedirs('plots')
 
-    pwndata=yaml.load(open(args.pwndata))[name]
-    ft1 = pwndata['ft1']
-    pulsar_position = SkyDir(*pwndata['cel'])
-    phase = roi.extra['phase']
-    pwnphase=roi.extra['pwnphase']
+        pwndata=yaml.load(open(args.pwndata))[name]
+        ft1 = pwndata['ft1']
+        pulsar_position = SkyDir(*pwndata['cel'])
+        phase = roi.extra['phase']
+        pwnphase=roi.extra['pwnphase']
 
-    print 'Making phaseogram'
+        print 'Making phaseogram'
 
-    plot_kwargs = dict(ft1=ft1, skydir=pulsar_position, phase_range=phase, 
-                       emin=pwnphase['optimal_emin'], emax=pwnphase['emax'], radius=pwnphase['optimal_radius'])
-    plot_phaseogram(title='Phaseogram for %s' % name, filename='plots/phaseogram_%s.png' % name, **plot_kwargs)
-    plot_phase_vs_time(title='Phase vs Time for %s' % name, filename='plots/phase_vs_time_%s.png' % name, **plot_kwargs)
+        plot_kwargs = dict(ft1=ft1, skydir=pulsar_position, phase_range=phase, 
+                           emin=pwnphase['optimal_emin'], emax=pwnphase['emax'], radius=pwnphase['optimal_radius'])
+        plot_phaseogram(title='Phaseogram for %s' % name, filename='plots/phaseogram_%s.png' % name, **plot_kwargs)
+        plot_phase_vs_time(title='Phase vs Time for %s' % name, filename='plots/phase_vs_time_%s.png' % name, **plot_kwargs)
 
-    new_sources = roi.extra['new_sources']
-    overlay_kwargs = dict(pulsar_position=pulsar_position, new_sources=new_sources)
-    
-    if followup == 'plots':
-        plots(roi, name, hypothesis, do_plots=True, do_tsmap=False, **overlay_kwargs)
-    elif followup == 'tsmaps':
-        plots(roi, name, hypothesis, do_plots=False, do_tsmap=True, **overlay_kwargs)
+        new_sources = roi.extra['new_sources']
+        overlay_kwargs = dict(pulsar_position=pulsar_position, new_sources=new_sources)
+        
+        if followup == 'plots':
+            plots(roi, name, hypothesis, do_plots=True, do_tsmap=False, **overlay_kwargs)
+        elif followup == 'tsmaps':
+            plots(roi, name, hypothesis, do_plots=False, do_tsmap=True, **overlay_kwargs)
 
 
-elif followup == 'variability':
+    elif followup == 'variability':
 
-    roi.print_summary()
-    roi.fit(use_gradient=False)
-    roi.print_summary()
+        roi.print_summary()
+        roi.fit(use_gradient=False)
+        roi.print_summary()
 
-    frozen  = freeze_far_away(roi, roi.get_source(name).skydir, args.max_free)
-    v = VariabilityTester(roi,name, nbins=36, 
-                          use_pointlike_ltcube=True, refit_background=True, refit_other_sources=True)
-    v.plot(filename='plots/variability_%s_hypothesis_%s.pdf' % (name,hypothesis))
-    unfreeze_far_away(roi, frozen)
+        frozen  = freeze_far_away(roi, roi.get_source(name).skydir, args.max_free)
+        v = VariabilityTester(roi,name, nbins=36, 
+                              use_pointlike_ltcube=True, refit_background=True, refit_other_sources=True)
+        v.plot(filename='plots/variability_%s_hypothesis_%s.pdf' % (name,hypothesis))
+        unfreeze_far_away(roi, frozen)
 
-    results = {hypothesis:{'variability':v.todict()}}
-    save_results(results,'results_%s_%s_%s.yaml' % (name,followup,hypothesis))
+        results = {hypothesis:{'variability':v.todict()}}
+        save_results(results,'results_%s_%s_%s.yaml' % (name,followup,hypothesis))
 
-elif followup == 'extul':
-    print 'Calculating extension upper limit'
+    elif followup == 'extul':
+        print 'Calculating extension upper limit'
 
-    r=roi.extension_upper_limit(which=name, confidence=0.95, spatial_model=Gaussian)
-    results = {hypothesis:{'pointlike':{'extension_upper_limit':r}}}
-    save_results(results,'results_%s_%s_%s.yaml' % (name,followup,hypothesis))
+        r=roi.extension_upper_limit(which=name, confidence=0.95, spatial_model=Gaussian)
+        results = {hypothesis:{'pointlike':{'extension_upper_limit':r}}}
+        save_results(results,'results_%s_%s_%s.yaml' % (name,followup,hypothesis))
--- lande/fermi/pipeline/pwncat2/analysis/main.py
+++ lande/fermi/pipeline/pwncat2/analysis/main.py
@@ -1,58 +1,24 @@
 #!/usr/bin/env python
-
 # This import has to come first
-from analyze_helper import pointlike_analysis,gtlike_analysis,save_results,\
-        plot_phaseogram,plot_phase_vs_time,all_energy,import_module
-from uw.pulsar.phase_range import PhaseRange
+from . helper import pointlike_analysis,save_results,import_module
 from lande.fermi.likelihood.tools import force_gradient
-from skymaps import SkyDir
 
 from uw.like.SpatialModels import Gaussian
 
 import os
 from os.path import join
-from glob import glob
 
 from lande.fermi.likelihood.parlimits import all_params_limited
 
 from setup_pwn import PWNRegion
-from argparse import ArgumentParser
 import yaml
 
 from collections import defaultdict
-import numpy as np
-np.seterr(all='ignore')
 
-
-def get_args():
-
-    parser = ArgumentParser()
-    parser.add_argument("--pwndata", required=True)
-    parser.add_argument("-p", "--pwnphase")
-    parser.add_argument("--no-phase-cut", default=False, action="store_true")
-    parser.add_argument("-n", "--name", required=True, help="Name of the pulsar")
-    parser.add_argument("--emin", default=1e2, type=float)
-    parser.add_argument("--binsperdec", default=4, type=int)
-    parser.add_argument("--emax", default=10**5.5, type=float)
-    parser.add_argument("--use-gradient", default=False, action="store_true")
-    parser.add_argument("--no-at-pulsar", default=False, action="store_true")
-    parser.add_argument("--no-point", default=False, action="store_true")
-    parser.add_argument("--no-extended", default=False, action="store_true")
-    parser.add_argument("--no-cutoff", default=False, action="store_true")
-    parser.add_argument("--no-savedir", default=False, action="store_true")
-    parser.add_argument("--max-free", default=5, type=float)
-    parser.add_argument("--modify", required=True)
-    args=parser.parse_args()
-    return args
-
-
-if __name__ == '__main__':
-    args = get_args()
-
+def main_pipeline(args):
     do_at_pulsar = not args.no_at_pulsar
     do_point = not args.no_point
-    do_extended = not args.no_extended
-    do_cutoff = not args.no_cutoff
+    do_extended = not args.no_extended 
 
     force_gradient(use_gradient=args.use_gradient)
 
@@ -61,16 +27,12 @@ if __name__ == '__main__':
     emax=args.emax
 
     pwnphase=yaml.load(open(args.pwnphase))[name]
-    if args.no_phase_cut:
-        phase = PhaseRange(0,1)
-    else:
-        phase=pwnphase['phase']
+    phase=pwnphase['phase']
 
     print 'phase = ',phase
 
     pwndata=yaml.load(open(args.pwndata))[name]
 
-    # nb, $PWD gets nicer paths then os.getcwd()
     savedir=None if args.no_savedir else join(os.getenv('PWD'),'savedir')
 
     results=r=defaultdict(lambda: defaultdict(dict))
