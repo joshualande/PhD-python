593b137724344754cd3dc6023368775fca506ff6
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2012-11-01 20:11:15 +0000

    update

--- lande/fermi/pipeline/pwncat2/analysis/builder.py
+++ lande/fermi/pipeline/pwncat2/analysis/builder.py
@@ -1,4 +1,4 @@
-from os.path import join
+from os.path import join, basename
 from textwrap import dedent
 from itertools import product
 
@@ -12,14 +12,15 @@ class PipelineBuilder(JobBuilder):
 
             for hypothesis in hypotheses:
                 for followup in followups:
-                    run = join(subdir,'followup_%s_%s.sh' % (hypothesis,followup))
+                    pwn = basename(subdir)
+                    run = join(subdir,'followup_%s_%s_%s.sh' % (pwn,followup,hypothesis))
                     open(run,'w').write("python %s %s --hypothesis=%s --followup=%s" % (code,args,hypothesis,followup))
 
             followup_all = join(self.savedir,'followup_all.sh')
             open(followup_all,'w').write(dedent("""
             #!/usr/bin/env bash
             submit_list=""
-            for pwn in PSR*; do
+            for pwn in `find . -maxdepth 1 -type d|xargs -i basename {}`; do
                 for hypothesis in at_pulsar point extended; do
                     if [ -e $pwn/roi_${hypothesis}_${pwn}.dat -a -e $pwn/results_${pwn}_pointlike_${hypothesis}.yaml ]; then
                         submit_list="$pwn/followup_${pwn}_*_${hypothesis}.sh $submit_list"
