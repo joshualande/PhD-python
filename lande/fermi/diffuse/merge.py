b90b8c5f007e2628234e55e2e1211d1562398aba
fdepalma <fdepalma@37a9682d-6443-41a2-8582-b44379b6e86f>
2013-02-04 16:47:57 +0000

    merge with two more option

--- lande/fermi/diffuse/merge.py
+++ lande/fermi/diffuse/merge.py
@@ -1,20 +1,23 @@
 import pyfits
-
+import os
+from subprocess import check_call
 from skymaps import DiffuseFunction
 
 from uw.like.Models import Constant
 from uw.like.roi_diffuse import DiffuseSource
 from uw.like.roi_monte_carlo import MCModelBuilder
 
-def merge_diffuse(diffuse_sources, scaling_model=None, mergefile=None, verbosity=False):
+def merge_diffuse(diffuse_sources, scaling_model=None, mergefile=None, verbosity=False, short_name=False , compress=False):
     """ merge diffuse files into one file. """
 
     for i in diffuse_sources:
         assert MCModelBuilder.isone(i.smodel)
         assert isinstance(i.dmodel[0], DiffuseFunction)
 
-
-    merged_name = 'merged(%s)' % (','.join([i.name for i in diffuse_sources]))
+    if not short_name:
+        merged_name = 'merged+%s' % ('+'.join([i.name for i in diffuse_sources]))
+    else:
+        merged_name = 'merged'
 
     if scaling_model is None:
         scaling_model = Constant()
@@ -26,7 +29,19 @@ def merge_diffuse(diffuse_sources, scaling_model=None, mergefile=None, verbosity
             print ' .. ',file
 
     if mergefile is None:
-        mergefile = 'merged.fits'
+        if not compress:
+            mergefile = 'merged.fits'
+        else:
+            mergefile = 'merged.fits.gz'
+
+    
+    if compress:
+        if mergefile.find(".gz")>0:
+            temp_file=mergefile.remove(".gz")
+        else:
+            temp_file=mergefile
+            mergefile=mergefile+".gz"
+    
 
     pf = [pyfits.open(i) for i in filenames]
 
@@ -36,8 +51,16 @@ def merge_diffuse(diffuse_sources, scaling_model=None, mergefile=None, verbosity
         if first['PRIMARY'].header != i['PRIMARY'].header:
             print 'WARNING: HEADER FILES DISAGREE. PERFORMING MERGE ANYWAY'
         first['PRIMARY'].data += i['PRIMARY'].data
-    first.writeto(mergefile, clobber=True)
 
+    if not compress:
+        first.writeto(mergefile, clobber=True)
+    else:
+        first.writeto(temp_file, clobber=True)
+        
+        if os.path.exists(mergefile):
+                os.remove(mergefile)
+        check_call(['gzip', temp_file])
+        
     merged_dmodel=DiffuseFunction(mergefile)
 
     new_source = DiffuseSource(
