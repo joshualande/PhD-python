cd3ffa3e786f94eaab651712ab0891321587be56
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2013-02-27 18:33:41 +0000

    update

--- lande/utilities/arrays.py
+++ lande/utilities/arrays.py
@@ -1,4 +1,35 @@
+import operator
+
 import numpy as np
+
+def recursive_allclose(a,b, *args, **wargs):
+    """ 
+        >>> np.allclose(1e-20, 1.1e-20)
+        True
+        >>> np.allclose(1e-20, 1e-5)
+        False
+        >>> recursive_allclose(dict(a=dict(b=1e-20)), dict(a=dict(b=1.1e-20)))
+        True
+        >>> recursive_allclose(dict(a=dict(b=1e-20)), dict(a=dict(b=1e-5)))
+        False
+        >>> recursive_allclose(dict(a=[1e-20]),dict(a=[1.1e-20]))
+        True
+        >>> recursive_allclose(dict(a=[1e-20]),dict(a=[1e-5]))
+        False
+        >>> recursive_allclose('cat','cat')
+        True
+        >>> recursive_allclose('cat','dog')
+        False
+    """
+    assert False,"This function was never finished"
+    if isinstance(a,dict):
+        assert isinstance(b,dict) and a.keys() == b.keys(), '%s, %s' % (a.keys(),b.keys())
+        return reduce(operator.and_,[recursive_allclose(a[i],b[i], *args, **wargs) for i in a.keys()])
+    elif isinstance(a,str):
+        assert isinstance(b,str), '%s, %s' % (type(a),type(b))
+        return a==b
+    else:
+        return np.allclose(a,b, *args, **wargs)
 
 def almost_equal(a,b, rtol=1e-05, atol=1e-08):
     return np.abs(a - b) <= (atol + rtol * np.abs(b))
