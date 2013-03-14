eec365830c3066c61ee9e407431c387c85b7c3a2
lande <lande@37a9682d-6443-41a2-8582-b44379b6e86f>
2013-03-14 23:41:48 +0000

    new

---
+++ lande/utilities/data.py
@@ -0,0 +1,16 @@
+from pandas import DataFrame
+
+
+def HDUtoPandas(hdu, key=None):
+    """ Convert a pyfits hdu to a pandas data structure where the indices in the DataFrame
+        are one of the columns in the fits file. """
+    names = list(hdu.data.names)
+
+    if key is not None: assert key in names
+    
+    # stip out vector columns (for now)
+    names = [str(name) for name in names if len(hdu.data[name].shape)==1]
+    df=DataFrame({n:hdu.data[n] for n in names}, index=names)
+        
+    if key is not None: df=df.set_index([key])
+    return df
